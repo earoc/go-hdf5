6238a6b89daa1ae0b826804f4026d1104737906b
Ashwin Purohit <purohit@gmail.com>
2015-12-17 17:56:17 -0700

    Add the linux hdf5 flags.

--- cgoflags.go
+++ cgoflags.go
@@ -1,6 +1,8 @@
 package hdf5
 
 // #cgo LDFLAGS: -lhdf5 -lhdf5_hl
+// #cgo linux CFLAGS: -I/usr/include/hdf5/serial/
+// #cgo linux LDFLAGS: -L/usr/lib/x86_64-linux-gnu/hdf5/serial/
 // #cgo darwin CFLAGS: -I/usr/local/include
 // #cgo darwin LDFLAGS: -L/usr/local/lib
 // #include "hdf5.h"
